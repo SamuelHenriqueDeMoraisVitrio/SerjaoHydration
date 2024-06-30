d933333c4ffc3a8c4f43cbbc6618ab71a71a0fb6
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-06-30 06:32:34 -0300

    teste3

--- teste3.lua
+++ teste3.lua
@@ -22,11 +22,13 @@ local function main(rq)
       return "botao Não foi informado", 404
     end
 
+    if botao == "10" then
+      return hy.replace_element_by_id("div", "<h1>" .. visor .. "</h1>")
+    end
+
     local novoVisor = visor .. botao
 
-    local novoInput = '<input id="num" value="' .. novoVisor .. '"/>'
-
-    return hy.replace_element_by_id("num", novoInput)
+    return hy.set_input_by_id("num", novoVisor)
   end
   return html(head(
                 script(
@@ -34,8 +36,18 @@ local function main(rq)
                 )
           ),
           body(
-            serjao.fragment('<input id="num" value="0"/>'),
-            button("1", {onclick=determinanum.call("{'selectnum': 1}")})
+                  div({id="div"}),
+                  serjao.fragment('<input id="num" value="0"/>'),
+                  button("1", {onclick=determinanum.call("{'selectnum': 1}")}),
+                  button("2", {onclick=determinanum.call("{'selectnum': 2}")}),
+                  button("3", {onclick=determinanum.call("{'selectnum': 3}")}),
+                  button("4", {onclick=determinanum.call("{'selectnum': 4}")}),
+                  button("5", {onclick=determinanum.call("{'selectnum': 5}")}),
+                  button("6", {onclick=determinanum.call("{'selectnum': 6}")}),
+                  button("7", {onclick=determinanum.call("{'selectnum': 7}")}),
+                  button("8", {onclick=determinanum.call("{'selectnum': 8}")}),
+                  button("9", {onclick=determinanum.call("{'selectnum': 9}")}),
+                  button("calc", {onclick=determinanum.call("{'selectnum': 10}")})
             )
           )
 end
